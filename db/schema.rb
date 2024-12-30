5aea7f6108bf35e8a4c191edb607db2b7f23750e
Minh <ngohoangnhatminh@gmail.com>
2024-12-30 15:54:07 +0800

    feat: update the db schema to include Categories table and Comments table and creating association between the tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_29_035557) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_30_070333) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "topic"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "category_id"
   end
 end
