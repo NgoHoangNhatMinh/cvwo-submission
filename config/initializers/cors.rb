9368c2027252839043e6130f3a8953a1874d27ba
Minh <ngohoangnhatminh@gmail.com>
2025-01-19 22:02:53 +0800

    fix: configure cors URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173",
-              "https://cvwoforum.netlify.app/"
+    origins "localhost:5173", "https://cvwoforum.netlify.app"
+    # origins "*" # Allow all
 
     resource "*",
       headers: :any,
